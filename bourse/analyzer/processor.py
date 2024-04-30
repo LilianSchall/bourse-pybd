c8c65431555c9b473f5cb028c0862ea9ca7eb4f7
Ahmed Hassayoune <ahmed.hassayoune@epita.fr>
2024-04-30 23:37:04 +0200

    Squashed commit of the following:
    commit 85e4c302a2e81ade4b2137cf4bc89f65236e837d
    Merge: 069971e e83be14
    Author: Lilian 'S3l4h' Schall <68446910+LilianSchall@users.noreply.github.com>
    Date:   Tue Apr 30 21:42:34 2024 +0200
    
        Merge pull request #9 from LilianSchall/feat/clean-stocks
    
    commit e83be14f14f153962670f8b6af58b74c26c01bf4
    Author: Guillaume Lalire <guillaume.lalire@epita.fr>
    Date:   Tue Apr 30 11:00:40 2024 +0200
    
        feat: remove useless stocks before committing
    
    commit 069971e176dbf8559e8f92769a0691476584722f
    Merge: a955693 0a669a3
    Author: Lilian 'S3l4h' Schall <68446910+LilianSchall@users.noreply.github.com>
    Date:   Tue Apr 30 00:08:33 2024 +0200
    
        Merge pull request #8 from LilianSchall/feat/restructure-analyzer
    
    commit 0a669a3dcff879accc3f293b9520758289f3ef62
    Author: Lilian Schall <lilian.schall@epita.fr>
    Date:   Tue Apr 30 00:06:25 2024 +0200
    
        feat: made huge refactor into separate classes, project is now quite readable

--- bourse/analyzer/processor.py
+++ bourse/analyzer/processor.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+
 
 class Processor:
     def __init__(self, log):
@@ -6,19 +8,21 @@ class Processor:
         self.day_batch = []
         self.daystocks_batch = []
         self.companies_batch = []
-        self.companies_save : None | pd.DataFrame = None
+        self.companies_save: None | pd.DataFrame = None
         self.log = log
 
     def __process_companies(self, df: pd.DataFrame | pd.Series, nb_companies: int):
         df.drop("symbol", axis=1, inplace=True)
         df.dropna(inplace=True)
-    
-        companies = df.reset_index()\
-            .drop_duplicates("symbol")\
-            .drop(columns=["last", "volume", "date"])\
+
+        companies = (
+            df.reset_index()
+            .drop_duplicates("symbol")
+            .drop(columns=["last", "volume", "date"])
             .reset_index(drop=True)
+        )
         companies.index.rename("id", inplace=True)
-    
+
         if self.companies_save is None:
             self.companies_save = companies
             return companies, nb_companies + len(companies)
@@ -29,7 +33,7 @@ class Processor:
             new_companies.reset_index(drop=True, inplace=True)
             new_companies.index.rename("id", inplace=True)
             new_companies.index += nb_companies
-    
+
             # saving new companies
             self.companies_save = pd.concat([self.companies_save, new_companies])
             # db.df_write(new_companies, "companies")
@@ -37,18 +41,20 @@ class Processor:
             return new_companies, nb_companies + len(new_companies)
 
     def __process_stocks(self, df: pd.DataFrame | pd.Series):
-        stocks = df.reset_index()\
-            .merge(self.companies_save
-                   .assign(cid=self.companies_save.index)[[ "symbol", "cid" ]],
-                   on="symbol", how="left")
+        stocks = df.reset_index().merge(
+            self.companies_save.assign(cid=self.companies_save.index)[
+                ["symbol", "cid"]
+            ],
+            on="symbol",
+            how="left",
+        )
         stocks.set_index("cid", inplace=True)
         stocks = stocks.drop(columns=["symbol", "name", "mid"])
-    
+
         stocks["value"] = stocks["last"].astype(str)
         stocks.drop(axis=1, labels="last", inplace=True)
-    
-        stocks["value"] = stocks["value"]\
-            .str.replace("\([a-zA-Z]\)| ", "", regex=True) 
+
+        stocks["value"] = stocks["value"].str.replace("\([a-zA-Z]\)| ", "", regex=True)
         stocks["value"] = stocks["value"].astype(float)
         return stocks
 
@@ -56,30 +62,51 @@ class Processor:
         # companies, stocks, daystocks, file_done, tags
         companies, nb_companies = self.__process_companies(df, nb_companies)
         stocks = self.__process_stocks(df)
-    
+
         self.companies_batch.append(companies)
         self.stocks_batch.append(stocks)
         self.day_batch.append(stocks)
-    
+
         return nb_companies
-    
+
     def process_daystocks(self, prev_date):
         self.log.debug(f"Group by {len(self.day_batch)} for daystocks")
         daystocks = pd.concat(self.day_batch, ignore_index=False)
-        daystocks = daystocks.groupby(['cid']).agg(date=pd.NamedAgg(column='value', aggfunc='first'),
-                                                   open=pd.NamedAgg(column='value', aggfunc='first'),
-                                                   close=pd.NamedAgg(column='value', aggfunc='last'),
-                                                   high=pd.NamedAgg(column='value', aggfunc='max'),
-                                                   low=pd.NamedAgg(column='value', aggfunc='min'),
-                                                   #volume=pd.NamedAgg(column='volume', aggfunc='sum'),
+        daystocks = daystocks.groupby(["cid"]).agg(
+            date=pd.NamedAgg(column="value", aggfunc="first"),
+            open=pd.NamedAgg(column="value", aggfunc="first"),
+            close=pd.NamedAgg(column="value", aggfunc="last"),
+            high=pd.NamedAgg(column="value", aggfunc="max"),
+            low=pd.NamedAgg(column="value", aggfunc="min"),
+            # volume=pd.NamedAgg(column='volume', aggfunc='sum'),
         )
-        daystocks['date'] = prev_date
+        daystocks["date"] = prev_date
         self.daystocks_batch.append(daystocks)
         self.day_batch = []
-    
+
     def reset_batch(self):
         self.stocks_batch = []
         self.companies_batch = []
         self.daystocks_batch = []
 
+    def clean_stocks(self, pool_size):
+        stocks = pd.concat(self.stocks_batch, ignore_index=False)
 
+        stocks.reset_index(inplace=True)
+        stocks.sort_values(["cid", "date"], inplace=True)
+
+        stocks["day"] = stocks["date"].dt.date
+
+        selected_columns = ["cid", "volume", "value", "day"]
+        df1 = stocks[
+            stocks[selected_columns].ne(stocks[selected_columns].shift(1)).any(axis=1)
+        ]
+        df2 = stocks[
+            stocks[selected_columns].ne(stocks[selected_columns].shift(-1)).any(axis=1)
+        ]
+        stocks = pd.concat([df1, df2.loc[~df2.index.isin(df1.index)]])
+
+        stocks.set_index("cid", drop=True, inplace=True)
+        stocks.drop(columns="day", inplace=True)
+
+        self.stocks_batch = np.array_split(stocks, pool_size)

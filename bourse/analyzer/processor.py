3a81acef65498adadec7550385295d12378d0c1c
Guillaume Lalire <guillaume.lalire@epita.fr>
2024-05-22 17:28:16 +0200

    doc: added comments in processor code

--- bourse/analyzer/processor.py
+++ bourse/analyzer/processor.py
@@ -56,6 +56,7 @@ class Processor:
             Process the dataframe and fill up the stocks table.
             @param df: the dataframe to process
         """
+        # add cid (company id) to stocks
         stocks = df.reset_index().merge(
             self.companies_save.assign(cid=self.companies_save.index)[
                 ["symbol", "cid"]
@@ -66,11 +67,15 @@ class Processor:
         stocks.set_index("cid", inplace=True)
         stocks = stocks.drop(columns=["symbol", "name", "mid"])
 
+        # rename "last" to "value" to follow database format 
         stocks["value"] = stocks["last"].astype(str)
         stocks.drop(axis=1, labels="last", inplace=True)
 
+        # remove letters in value field and convert to float
         stocks["value"] = stocks["value"].str.replace("\([a-zA-Z]\)| ", "", regex=True)
         stocks["value"] = stocks["value"].astype(float)
+
+        # return processed stocks
         return stocks
 
     def process_dataframe(self, df: pd.DataFrame | pd.Series, nb_companies: int):
@@ -96,17 +101,25 @@ class Processor:
             @param prev_date: the date of the previous file processed.
         """
         self.log.debug(f"Group by {len(self.day_batch)} for daystocks")
+
+        # concat all stocks from this day
         daystocks = pd.concat(self.day_batch, ignore_index=False)
+
+        # compute relevant daystocks infos through aggregation
         daystocks = daystocks.groupby(["cid"]).agg(
             date=pd.NamedAgg(column="value", aggfunc="first"),
             open=pd.NamedAgg(column="value", aggfunc="first"),
             close=pd.NamedAgg(column="value", aggfunc="last"),
             high=pd.NamedAgg(column="value", aggfunc="max"),
             low=pd.NamedAgg(column="value", aggfunc="min"),
-            # volume=pd.NamedAgg(column='volume', aggfunc='sum'),
         )
+        
+        # set the date
         daystocks["date"] = prev_date
+
+        # add to daystocks batch
         self.daystocks_batch.append(daystocks)
+        # clear saved stocks from current day
         self.day_batch = []
 
     def reset_batch(self):
@@ -119,6 +132,7 @@ class Processor:
             Clean the stock batch by removing repeating stock values, so that
             we store fewer value in database.
         """
+        # concat all stocks batchs
         stocks = pd.concat(self.stocks_batch, ignore_index=False)
 
         stocks.reset_index(inplace=True)
@@ -127,17 +141,25 @@ class Processor:
         stocks["day"] = stocks["date"].dt.date
 
         selected_columns = ["cid", "volume", "value", "day"]
+        # select stocks where there is a change compared to the previous row
         df1 = stocks[
             stocks[selected_columns]
                 .ne(stocks[selected_columns]
-                .shift(1)).any(axis=1)
+                .shift(1))
+                .any(axis=1)
         ]
+        # select stocks where there is a change compared to the next row
         df2 = stocks[
-            stocks[selected_columns].ne(stocks[selected_columns].shift(-1)).any(axis=1)
+            stocks[selected_columns]
+                .ne(stocks[selected_columns]
+                .shift(-1))
+                .any(axis=1)
         ]
+        # concat the two df without duplicates
         stocks = pd.concat([df1, df2.loc[~df2.index.isin(df1.index)]])
 
         stocks.set_index("cid", drop=True, inplace=True)
         stocks.drop(columns="day", inplace=True)
 
+        # split clean stocks into batches
         self.stocks_batch = np.array_split(stocks, pool_size)
